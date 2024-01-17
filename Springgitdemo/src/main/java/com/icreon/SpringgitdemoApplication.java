2c3d4def61b6b284075f224d2de71d9db67e07b3
Vinayak <vinayaknikam60@gmail.com>
2024-01-17 11:30:48 +0530

    code commit

---
+++ Springgitdemo/src/main/java/com/icreon/SpringgitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.icreon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgitdemoApplication.class, args);
+	}
+
+}
---
+++ Springgitdemo/src/test/java/com/icreon/SpringgitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icreon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringgitdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
